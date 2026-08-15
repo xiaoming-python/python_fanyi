65885ee08ad33c8102e28e76cd57314f3f00d154
xiaomin <tangjiacan0927@gmail.com>
2026-08-15 13:28:07 +0800

    Update 翻译工具.py

--- 练习1/翻译工具.py
+++ 练习1/翻译工具.py
@@ -9,7 +9,7 @@ import time
 
 # ====================== 请修改这里 ======================
 YOUDAO_URL = "https://openapi.youdao.com/api"
-APP_KEY = "06de3f42c2e139eb"  # 替换成你创建应用后得到的 appKey
+APP_KEY = "you_apikey"  # 替换成你创建应用后得到的 appKey
 APP_SECRET = "XfVPqY4BhMNTiTHHMgIkDklLCaNOo9bh"  # 替换成你创建应用后得到的 appSecret
 
 
